9e6614efcdd3bd6a108324c3f6a4ef934b3158e7
albert-sun <albert.h.sun@gmail.com>
2021-10-21 01:15:01 -0500

    initial evga

---
+++ source/background/module_evga.ts
@@ -0,0 +1,3 @@
+// Scrapes relevant tokens and product information from given EVGA store page
+// Note that sorting method (Low-High, etc.) matter because items are indexed
+async function scrapeEVGA(url: string): 
